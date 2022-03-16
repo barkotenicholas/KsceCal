6423a17166abdcb4071fb71e9c48d314c5106181
barkotenicholas <barkotenicholas@gmail.com>
2022-03-16 18:21:27 +0300

    added switch clause

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -2,6 +2,5 @@ import { Calculator } from './calculator';
 
 describe('Calculator', () => {
   it('should create an instance', () => {
-    expect(new Calculator()).toBeTruthy();
   });
 });
--- src/app/calculator.ts
+++ src/app/calculator.ts
@@ -8,7 +8,7 @@ export class Calculator {
     chem :string;
     biz:string;
     agri:string
-
+    sum!: number;
     constructor(name:string,math:string,eng:string,kisw:string,bio : string,chem :string, biz:string,agri:string){
         this.name = name;
         this.math = math;
@@ -20,5 +20,48 @@ export class Calculator {
         this.agri = agri;
     }
 
+
+    add(){
+
+       
+        switch (this.math) {
+            case 'A':
+                this.sum +=12
+                break;
+            case 'A-':
+                this.sum +=11
+                break;
+            case 'B+':
+                this.sum +=10
+                break;
+            case 'B':
+                this.sum +=9
+                break;
+            case 'B-':
+                this.sum +=8
+                break;
+            case 'C+':
+                this.sum +=7
+                break;
+            case 'C-':
+                this.sum +=6
+                break;
+            case 'D+':
+                this.sum +=5
+            break;
+            case 'D':
+                this.sum +=4
+                break;
+            case 'D-':
+                this.sum +=3
+                break;
+        
+            default:
+                this.sum +=0
+                break;
+        }
+        
+
+    }
     
 }
