1d8ef95a88bc5c40a05e85f50057b01b27fb540a
barkotenicholas <barkotenicholas@gmail.com>
2022-03-16 13:10:40 +0300

    Added grades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,40 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   name!: string;
+  math!: string;
+  eng!: string;
+  kisw!: string;
+  bio!: string;
+  chem!: string;
+  his!: string;
+  biz!: string;
+  agri!: string;
+  geo!: string;
+  phy!: string;
+  ho!:string;
+
   submit(details: any){
     console.log(details)
+
     console.log(this.name)
+
+    console.log(this.math)
+
+    console.log(this.geo)
+
+    console.log(this.kisw)
+
+    console.log(this.eng)
+
+    console.log(this.his)
+
+    console.log(this.geo)
+
+    console.log(this.ho)
+
+    console.log(this.agri)
+
+    console.log(this.biz)
+
   }
 }
